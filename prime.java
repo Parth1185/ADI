4c6d346e495bd7005a3d0ec8bd86bac9ee70a53c
Parth <parthbhandari17851@gmail.com>
2025-10-07 17:29:08 +0530

    second commit

--- prime.java
+++ prime.java
@@ -1,5 +1,13 @@
-class prime{
+class prime {
+    public static boolean primeCheck(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("Is " + 23 + " prime : " + primeCheck(36));
     }
 }
---
+++ printPrime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class printPrime {
+
+    public static boolean primeCheck(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static List<Integer> fun1(int n) {
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (primeCheck(i)) primes.add(i);
+        }
+        return primes;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun1(100));
+    }
+}
